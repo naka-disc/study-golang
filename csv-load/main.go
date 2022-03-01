3ce4359ad9811ab30b19e775fa8edebfe18ad713
naka-disc <99717730+naka-disc@users.noreply.github.com>
2022-03-02 02:34:24 +0900

    スペースからTabに変換

--- csv-load/main.go
+++ csv-load/main.go
@@ -1,41 +1,42 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "log"
-    "io"
-    "encoding/csv"
-    "golang.org/x/text/encoding/japanese"
-    "golang.org/x/text/transform"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"golang.org/x/text/encoding/japanese"
+	"golang.org/x/text/transform"
 )
 
 func main() {
-    // ファイルを展開
-    file, err := os.Open("resources/c03.csv")
+	// ファイルを展開
+	file, err := os.Open("resources/c03.csv")
 
-    // ファイル展開に失敗したらログ出力して終了
-    if err != nil {
-        log.Fatal(err)
-    }
+	// ファイル展開に失敗したらログ出力して終了
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    // CSVのリーダーを作成
-    // TODO: Shift-JISのみの対応になっているので要修正（文字コード検知など）
-    reader := csv.NewReader(transform.NewReader(file, japanese.ShiftJIS.NewDecoder()))
+	// CSVのリーダーを作成
+	// TODO: Shift-JISのみの対応になっているので要修正（文字コード検知など）
+	reader := csv.NewReader(transform.NewReader(file, japanese.ShiftJIS.NewDecoder()))
 
-    for {
-        // リーダーから一行ずつ取得していく
-        record, err := reader.Read()
-        // errに終端記号が格納されていれば読み込み終了
-        if err == io.EOF {
-            break
-        }
-        // その他、何かしらのエラーがあればログ出力して終了
-        if err != nil {
-            log.Fatal(err)
-        }
-        // 取得した行を出力
-        fmt.Println(record)
-    }
+	for {
+		// リーダーから一行ずつ取得していく
+		record, err := reader.Read()
+		// errに終端記号が格納されていれば読み込み終了
+		if err == io.EOF {
+			break
+		}
+		// その他、何かしらのエラーがあればログ出力して終了
+		if err != nil {
+			log.Fatal(err)
+		}
+		// 取得した行を出力
+		fmt.Println(record)
+	}
 
 }
